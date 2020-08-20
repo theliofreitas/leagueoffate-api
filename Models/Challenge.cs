77e6642c825c2c24c1b8bd8cf7f632109084b545
Thélio Freitas <marquesthelio@hotmail.com>
2020-08-19 22:40:04 -0300

    Setting required fields

--- Models/Challenge.cs
+++ Models/Challenge.cs
@@ -1,6 +1,7 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace LeagueOfFateApi.Models
 {
@@ -9,6 +10,7 @@ namespace LeagueOfFateApi.Models
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
 
+    [Required]
     public string SummonerName { get; set; }
 
     public string SummonerId { get; set; }
@@ -17,6 +19,7 @@ namespace LeagueOfFateApi.Models
 
     public long? MatchId { get; set; }
     
+    [Required, MinLength(1)]
     public List<Criterial> Criterials { get; set; }
   }
 }
--- Models/Criterial.cs
+++ Models/Criterial.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LeagueOfFateApi.Models 
 {
   public class Criterial {
+    [Required]
     public string Field { get; set; }
 
+    [Required]
     public string Operator { get; set; }
 
+    [Required]
     public string Value { get; set; }
 
     public bool Result { get; set; }
